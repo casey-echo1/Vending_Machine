0933903b08ca50d2ac4eabd190b30efa405887b5
Casey Echternacht <casechtern@yahoo.com>
2024-07-30 12:43:42 -0600

    added method to send notification when a product runs out of stock

--- src/main/java/org/example/VendRunner.java
+++ src/main/java/org/example/VendRunner.java
@@ -1,40 +1,28 @@
 package org.example;
 
+import java.io.IOException;
+
 public class VendRunner {
 
 	public static void main(String[] args) {
 
 
-		/*
-		makeSureIsValid() throws Exceptions A, B C,{
-			try {
-
-			} catch {
-			}
-		 */
-
 
 		try {
 			Snack snack1 = new Snack("Snickers", 2.00, true);
-			Snack snack2 = new Snack("", 2.50, true);
-			Snack snack3 = new Snack("Chips", -2.75, true);
-			Snack snack4 = new Snack(null, 2.75, true);
-
-			Beverage b1 = new Beverage("Red Bull", 3.50, 4.5);
-			Beverage b2 = new Beverage("Green Bull", -3.75, 4.5);
-			Beverage b3 = new Beverage("", 3.75, 4.5);
-			Beverage b4 = new Beverage(null, 3.75, 4.5);
-
 
 			Slot<Snack> slot1 = new Slot<Snack>(snack1, 3);
 
 
 			VendingMachine vendingMachine = new VendingMachine();
 			vendingMachine.addProduct("F1", slot1);
+			vendingMachine.dispenseProduct("F1");
+			vendingMachine.dispenseProduct("F1");
+			vendingMachine.dispenseProduct("F1");
+			vendingMachine.dispenseProduct("F1");
 
-		} catch (Exception e) {
+		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		}
-
 	}
 }
--- src/main/java/org/example/VendingMachine.java
+++ src/main/java/org/example/VendingMachine.java
@@ -1,5 +1,10 @@
 package org.example;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -22,7 +27,7 @@ public class VendingMachine {
 		slots.put(code, slot);
 	}
 
-	public void dispenseProduct(String code) {
+	public void dispenseProduct(String code) throws IOException {
 		if(code == null || code.isEmpty()) {
 			throw new IllegalArgumentException("Code cannot be empty or null");
 		}
@@ -31,16 +36,30 @@ public class VendingMachine {
 				Slot<? extends Product> slot = this.slots.get(code);
 				slot.setQuantity(this.slots.get(code).getQuantity() - 1);
 				System.out.println(slot.getProduct().getName());
+				printReceipt(slot.getProduct());
 			} else {
 				System.out.println("Product Not Available");
+				// notify vendor that product is out of stock
+				sendVendorNotification(this.slots.get(code).getProduct());
 			}
 		} else {
 			System.out.println("Invalid Input");
 		}
 	}
+	/*
+		this method creates a file with timestamp and name of product that is out of stock
+	 */
+	public void sendVendorNotification(Product product) throws IOException {
+		String location = "notification_"+product.getName()+".txt";
 
-	// "F2"
-	// if(input == "F2"
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(location, true))) {
+			writer.newLine();
+			writer.write(product.getName() + " is out of Stock!!!");
+			writer.newLine();
+			writer.write(LocalDateTime.now().toString());
+		}
+	}
+
 
 	public void displayProducts() {
 		//display all available products
@@ -48,6 +67,20 @@ public class VendingMachine {
 		//Slot{product=Snack{SnickersPrice= 2.0isVegan=true}, quantity=3}
 		// what we WANT
 		//F1 {product=Snack{SnickersPrice= 2.0isVegan=true}, quantity=3}
+	}
+	public void printReceipt(Product product) throws IOException {
+		String location = "receipt.txt";
+		//String timeStamp = LocalDateTime.now().toString();
+		double price = product.getPrice();
+		String productName = product.getName();
+
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(location, true))) {
+			bw.write("Receipt " + LocalDateTime.now().toString() + "\n");
+			bw.newLine();
+			bw.write("Product Name: " + productName);
+			bw.newLine();
+			bw.write("Product Price: " + price);
+		}
 	}
 
 	@Override
